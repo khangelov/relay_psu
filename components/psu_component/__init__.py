d1abfa57421a0ff623d55dbf36683a5cdf717493
khangelov <40702364+khangelov@users.noreply.github.com>
2025-04-27 20:54:22 +0300

    Chat

--- components/psu_component/__init__.py
+++ components/psu_component/__init__.py
@@ -1,56 +1,24 @@
-from esphome.components import polling_component
-from esphome.components import sensor
-import esphome.config_validation as cv
 import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import polling_component  # ✅ Important
+from esphome import automation
 from esphome.const import CONF_ID
 
-CONF_VOLT_IN = "volt_in"
-CONF_AMP_IN = "amp_in"
-CONF_WATT_IN = "watt_in"
-CONF_VOLT_OUT = "volt_out"
-CONF_AMP_OUT = "amp_out"
-CONF_WATT_OUT = "watt_out"
-CONF_INTERNAL_TEMP = "internal_temp"
-CONF_FAN_RPM = "fan_rpm"
+psu_component_ns = cg.namespace('psu_component')
+PSUComponent = psu_component_ns.class_('PSUComponent', cg.Component, polling_component.PollingComponent)
 
-# Define the namespace and component class
-psu_sensor_ns = cg.esphome_ns.namespace("PSUSensor")
-PSUSensorComponent = psu_sensor_ns.class_("PSUSensor", cg.PollingComponent)
+CONF_RELAY_PIN = "relay_pin"
 
-# Configuration schema for the component
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(PSUSensorComponent),
-        cv.Optional(CONF_VOLT_IN): sensor.sensor_schema(unit_of_measurement="V", accuracy_decimals=2),
-        cv.Optional(CONF_AMP_IN): sensor.sensor_schema(unit_of_measurement="A", accuracy_decimals=2),
-        cv.Optional(CONF_WATT_IN): sensor.sensor_schema(unit_of_measurement="W", accuracy_decimals=2),
-        cv.Optional(CONF_VOLT_OUT): sensor.sensor_schema(unit_of_measurement="V", accuracy_decimals=2),
-        cv.Optional(CONF_AMP_OUT): sensor.sensor_schema(unit_of_measurement="A", accuracy_decimals=2),
-        cv.Optional(CONF_WATT_OUT): sensor.sensor_schema(unit_of_measurement="W", accuracy_decimals=2),
-        cv.Optional(CONF_INTERNAL_TEMP): sensor.sensor_schema(unit_of_measurement="°C", accuracy_decimals=2),
-        cv.Optional(CONF_FAN_RPM): sensor.sensor_schema(unit_of_measurement="RPM", accuracy_decimals=0),
-    }
-).extend(cv.polling_component_schema("60s"))  # Update every 60 seconds
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(PSUComponent),
+    cv.Required(CONF_RELAY_PIN): cv.pin,
+}).extend(cv.COMPONENT_SCHEMA).extend(polling_component.POLLING_COMPONENT_SCHEMA)
 
 async def to_code(config):
-    # Create a new instance of PSUSensorComponent
     var = cg.new_Pvariable(config[CONF_ID])
+
+    await cg.register_component(var, config)
     await polling_component.register_polling_component(var, config)
 
-    # Register each sensor
-    for key, attr in [
-        (CONF_VOLT_IN, "volt_in_sensor"),
-        (CONF_AMP_IN, "amp_in_sensor"),
-        (CONF_WATT_IN, "watt_in_sensor"),
-        (CONF_VOLT_OUT, "volt_out_sensor"),
-        (CONF_AMP_OUT, "amp_out_sensor"),
-        (CONF_WATT_OUT, "watt_out_sensor"),
-        (CONF_INTERNAL_TEMP, "internal_temp_sensor"),
-        (CONF_FAN_RPM, "fan_rpm_sensor"),
-    ]:
-        if key in config:
-            sens = await sensor.new_sensor(config[key])
-            cg.add(getattr(var, f"set_{attr}")(sens))
-
-    # Add I2C dependency (Wire library)
-    cg.add_library("Wire", None)
+    relay_pin = await cg.gpio_pin_expression(config[CONF_RELAY_PIN])
+    cg.add(var.set_relay_pin(relay_pin))
